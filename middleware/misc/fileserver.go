17aa23419421a7bc31e5d0a3f3db448d6e818602
Jarlyyn <jarlyyn@qq.com>
2018-02-10 14:39:23 +0800

    remove file server

--- middleware/misc/fileserver.go
+++ middleware/misc/fileserver.go
@@ -4,47 +4,8 @@ import (
 	"net/http"
 	"os"
 	"path"
-	"strings"
 )
 
-type fileFilesystem struct {
-	Fs http.FileSystem
-}
-
-func (fs fileFilesystem) Open(name string) (http.File, error) {
-	f, err := fs.Fs.Open(name)
-	if err != nil {
-		return nil, err
-	}
-
-	stat, err := f.Stat()
-	if stat.IsDir() {
-		err = f.Close()
-		if err != nil {
-			return nil, err
-		}
-		return fs.Open(path.Join(name, "index.html"))
-	}
-
-	return f, nil
-}
-func ServePrefixedFiles(prefix string, root http.FileSystem) http.HandlerFunc {
-	fs := fileFilesystem{
-		Fs: root,
-	}
-	return func(w http.ResponseWriter, r *http.Request) {
-		url := r.URL.Path
-		filepath := strings.TrimPrefix(url, prefix)
-
-		if len(url) == len(filepath) {
-			http.NotFound(w, r)
-			return
-		}
-		r.URL.Path = filepath
-		http.FileServer(fs).ServeHTTP(w, r)
-
-	}
-}
 func ServeFile(path string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, path)
