b0d4b64220591474d8628a9d54bcac3a9017e9ee
Jarlyyn <jarlyyn@qq.com>
2018-08-05 02:31:43 +0800

    update

--- util/httpserver/config.go
+++ util/httpserver/config.go
@@ -2,14 +2,31 @@ package httpserver
 
 import (
 	"net"
+	"net/http"
+	"time"
 )
 
 type Config struct {
-	Net     string
-	Addr    string
-	BaseURL string
+	Net                       string
+	Addr                      string
+	BaseURL                   string
+	ReadTimeoutInSecond       int64
+	ReadHeaderTimeoutInSecond int64
+	WriteTimeoutInSecond      int64
+	IdleTimeoutInSecond       int64
+	MaxHeaderBytes            int
 }
 
+func (c *Config) Server() *http.Server {
+	server := &http.Server{
+		ReadTimeout:       time.Duration(c.ReadTimeoutInSecond) * time.Second,
+		ReadHeaderTimeout: time.Duration(c.ReadHeaderTimeoutInSecond) * time.Second,
+		WriteTimeout:      time.Duration(c.WriteTimeoutInSecond) * time.Second,
+		IdleTimeout:       time.Duration(c.IdleTimeoutInSecond) * time.Second,
+		MaxHeaderBytes:    c.MaxHeaderBytes,
+	}
+	return server
+}
 func (c *Config) Listen() (net.Listener, error) {
 	return net.Listen(c.Net, c.Addr)
 }
