6846595db1074d061a9cfbe5f2a5b6836e172827
Jarlyyn <jarlyyn@qq.com>
2018-09-12 02:43:42 +0800

    update

--- captcha/captcha.go
+++ captcha/captcha.go
@@ -7,8 +7,8 @@ import (
 
 type driver interface {
 	Type() string
-	Config(w http.ResponseWriter, r *http.Request) (json.RawMessage, error)
-	Reset(w http.ResponseWriter, r *http.Request) (json.RawMessage, error)
+	Config(scene string, w http.ResponseWriter, r *http.Request) (json.RawMessage, error)
+	Reset(scene string, w http.ResponseWriter, r *http.Request) (json.RawMessage, error)
 	Verify(r *http.Request, token string) (bool, error)
 }
 
@@ -19,79 +19,85 @@ type CommonOutput struct {
 }
 
 func New() *Captcha {
-	return &Captcha{}
+	return &Captcha{
+		DisabledScenes: map[string]bool{},
+	}
 }
-func defaultEnabledChecker(captcha *Captcha, w http.ResponseWriter, r *http.Request) (bool, error) {
+func defaultEnabledChecker(captcha *Captcha, scene string, w http.ResponseWriter, r *http.Request) (bool, error) {
 	return true, nil
 }
 
 type Captcha struct {
 	driver         driver
 	Disabled       bool
-	EnabledChecker func(captcha *Captcha, w http.ResponseWriter, r *http.Request) (bool, error)
+	DisabledScenes map[string]bool
+	EnabledChecker func(captcha *Captcha, scene string, w http.ResponseWriter, r *http.Request) (bool, error)
 }
 
-func (c *Captcha) EnabledCheck(w http.ResponseWriter, r *http.Request) (bool, error) {
+func (c *Captcha) EnabledCheck(scene string, w http.ResponseWriter, r *http.Request) (bool, error) {
 	if c.Disabled {
 		return false, nil
 	}
-	return c.EnabledChecker(c, w, r)
+	return c.EnabledChecker(c, scene, w, r)
 }
 
-func (c *Captcha) ConfigAction(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	enabled, err := c.EnabledCheck(w, r)
-	if err != nil {
-		panic(err)
-	}
-	output := CommonOutput{
-		Type:    c.driver.Type(),
-		Enabled: enabled,
-	}
-	if enabled {
-		config, err := c.driver.Config(w, r)
+func (c *Captcha) ConfigJSONAction(scene string) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		enabled, err := c.EnabledCheck(scene, w, r)
 		if err != nil {
 			panic(err)
 		}
-		output.Config = config
-	}
-	outputJSON, err := json.Marshal(output)
-	if err != nil {
-		panic(err)
-	}
-	_, err = w.Write(outputJSON)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func (c *Captcha) ResetAction(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	enabled, err := c.EnabledCheck(w, r)
-	if err != nil {
-		panic(err)
-	}
-	output := CommonOutput{
-		Type:    c.driver.Type(),
-		Enabled: enabled,
-	}
-	if enabled {
-		config, err := c.driver.Reset(w, r)
+		output := CommonOutput{
+			Type:    c.driver.Type(),
+			Enabled: enabled,
+		}
+		if enabled {
+			config, err := c.driver.Config(scene, w, r)
+			if err != nil {
+				panic(err)
+			}
+			output.Config = config
+		}
+		outputJSON, err := json.Marshal(output)
 		if err != nil {
 			panic(err)
 		}
-		output.Config = config
-	}
-	outputJSON, err := json.Marshal(output)
-	if err != nil {
-		panic(err)
-	}
-	_, err = w.Write(outputJSON)
-	if err != nil {
-		panic(err)
+		_, err = w.Write(outputJSON)
+		if err != nil {
+			panic(err)
+		}
 	}
 }
 
-func (c *Captcha) Verify(r *http.Request, token string) (bool, error) {
+func (c *Captcha) ResetJSONAction(scene string) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		enabled, err := c.EnabledCheck(scene, w, r)
+		if err != nil {
+			panic(err)
+		}
+		output := CommonOutput{
+			Type:    c.driver.Type(),
+			Enabled: enabled,
+		}
+		if enabled {
+			config, err := c.driver.Reset(scene, w, r)
+			if err != nil {
+				panic(err)
+			}
+			output.Config = config
+		}
+		outputJSON, err := json.Marshal(output)
+		if err != nil {
+			panic(err)
+		}
+		_, err = w.Write(outputJSON)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+func (c *Captcha) Verify(r *http.Request, scene string, token string) (bool, error) {
 	return c.driver.Verify(r, token)
 }
