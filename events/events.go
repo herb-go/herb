d2d6f6b4311d43d3ed0fc8757cc6308c6003add3
Jarlyyn <jarlyyn@qq.com>
2018-11-14 21:43:32 +0800

    update

--- events/events.go
+++ events/events.go
@@ -4,53 +4,67 @@ import (
 	"sync"
 )
 
+//NewEvent create new event.
 func NewEvent() *Event {
 	return &Event{}
 }
 
+//Type event type
 type Type string
 
+//NewEvent create event wth type.
 func (t Type) NewEvent() *Event {
 	return &Event{
 		Type: t,
 	}
 }
 
+// Hanlder event handler
 type Hanlder func(*Event)
 
+//Event event struct
 type Event struct {
 	Type   Type
 	Target string
 	Data   interface{}
 }
 
+//WithTarget set event target and return event
 func (e *Event) WithTarget(target string) *Event {
 	e.Target = target
 	return e
 }
 
+//WithData set event data and return event
 func (e *Event) WithData(data interface{}) *Event {
 	e.Data = data
 	return e
 }
 
+//WithType set event type and return event
 func (e *Event) WithType(t Type) *Event {
 	e.Type = t
 	return e
 }
 
-type Events struct {
+//EventService event service which handle evetns
+type EventService struct {
 	Hanlders map[Type][]Hanlder
 	lock     sync.RWMutex
 }
 
-func (e *Events) NewEvent() *Event {
+//NewEvent create new event
+func (e *EventService) NewEvent() *Event {
 	return &Event{}
 }
-func (e *Events) Emit(event *Event) {
+
+//Emit emit event to event service
+func (e *EventService) Emit(event *Event) {
 	e.Trigger(event)
 }
-func (e *Events) On(t Type, hanlder Hanlder) {
+
+//On register event handlter to given type.
+func (e *EventService) On(t Type, hanlder Hanlder) {
 	e.lock.Lock()
 	defer e.lock.Unlock()
 
@@ -60,44 +74,52 @@ func (e *Events) On(t Type, hanlder Hanlder) {
 	e.Hanlders[t] = append(e.Hanlders[t], hanlder)
 }
 
-func (e *Events) Trigger(event *Event) bool {
+//Trigger trigger a event to service.
+//return if event is hanlded
+func (e *EventService) Trigger(event *Event) bool {
 	e.lock.RLock()
 	defer e.lock.RUnlock()
 	if e.Hanlders[event.Type] == nil {
-		return true
+		return false
 	}
 	for k := range e.Hanlders[event.Type] {
 		go e.Hanlders[event.Type][k](event)
 	}
-	return false
+	return true
 }
 
-func New() *Events {
-	e := &Events{
+//New create New EventsService
+func New() *EventService {
+	e := &EventService{
 		Hanlders: map[Type][]Hanlder{},
 	}
 	return e
 }
 
+//WrapEmit return a default service event emitter with given type
 func WrapEmit(t Type) func(*Event) {
 	return func(e *Event) {
 		e.Type = t
-		DefaultEvents.Emit(e)
+		Emit(e)
 	}
 }
 
+//WrapOn return a defalut service hanlder registeror of give event type
 func WrapOn(t Type) func(Hanlder) {
 	return func(hanlder Hanlder) {
-		DefaultEvents.On(t, hanlder)
+		On(t, hanlder)
 	}
 }
 
-var DefaultEvents = New()
+//DefaultEventService default event service.
+var DefaultEventService = New()
 
+//On register default service event handlter to given type.
 func On(t Type, hanlder Hanlder) {
-	DefaultEvents.On(t, hanlder)
+	DefaultEventService.On(t, hanlder)
 }
 
+//Emit emit event to default event service
 func Emit(event *Event) {
-	DefaultEvents.Emit(event)
+	DefaultEventService.Emit(event)
 }
---
+++ events/events_test.go
@@ -0,0 +1,57 @@
+package events
+
+import (
+	"testing"
+	"time"
+)
+
+func TestEvents(t *testing.T) {
+	eventtype := Type("testtype")
+	testtarget := "target"
+	testdata := 15
+	e := NewEvent()
+	if e.Type != "" || e.Data != nil || e.Target != "" {
+		t.Error(e)
+	}
+	e.WithType(eventtype).WithData(testdata).WithTarget(testtarget)
+	d, ok := e.Data.(int)
+	if e.Type != eventtype || ok != true || d != testdata || e.Target != testtarget {
+		t.Error(e)
+	}
+}
+func TestDefaultEvents(t *testing.T) {
+	eventtype := Type("testtype")
+	eventtype2 := Type("testtype2")
+	e := eventtype.NewEvent()
+	e2 := eventtype2.NewEvent()
+	if e.Type == eventtype2 {
+		t.Error(e)
+	}
+	if e.Type != eventtype {
+		t.Error(e)
+	}
+	e3 := DefaultEventService.NewEvent()
+	if e3.Type != "" {
+		t.Error(e)
+	}
+	var result1 bool
+	var result2 bool
+
+	//test register event handler again.
+	EmittEvenentType := WrapEmit(eventtype)
+	OnEventType := WrapOn(eventtype)
+	OnEventType(func(e *Event) {
+		result1 = true
+	})
+	OnEventType(func(e *Event) {
+		result2 = true
+	})
+	EmittEvenentType(e)
+	time.Sleep(1 * time.Millisecond)
+	if result1 != true || result2 != true {
+		t.Error(e)
+	}
+	if DefaultEventService.Trigger(e2) != false {
+		t.Error(e2)
+	}
+}
