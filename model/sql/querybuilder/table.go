6a121ced40b3c0bfb7100188a042655196a8dd78
jarlyyn <jarlyyn@qq.com>
2019-01-16 00:26:48 +0800

    update

---
+++ model/sql/querybuilder/table.go
@@ -0,0 +1,76 @@
+package querybuilder
+
+type DBTable interface {
+	DB
+	TableName() string
+	Alias() string
+	Driver() string
+}
+
+type Table struct {
+	DBTable
+}
+
+//QueryBuilder return querybuilder of  table
+func (t *Table) QueryBuilder() *Builder {
+	return &Builder{
+		Driver: t.DBTable.Driver(),
+	}
+}
+
+//FieldAlias return field name with table alias.
+func (t *Table) FieldAlias(field string) string {
+	return t.Alias() + "." + field
+}
+
+//NewSelect : create  select query for table
+func (t *Table) NewSelect() *Select {
+	Select := t.QueryBuilder().NewSelect()
+	Select.From.Add(t.TableName())
+	return Select
+}
+
+//NewInsert : new insert query for table node
+func (t *Table) NewInsert() *Insert {
+	Insert := t.QueryBuilder().NewInsert(t.TableName())
+	return Insert
+
+}
+
+//NewUpdate : new update query for table
+func (t *Table) NewUpdate() *Update {
+	Update := t.QueryBuilder().NewUpdate(t.TableName())
+	Update.Update.SetAlias(t.TableName())
+	return Update
+}
+
+//NewDelete : build delete query for table node
+func (t *Table) NewDelete() *Delete {
+	Delete := t.QueryBuilder().NewDelete(t.TableName())
+	Delete.Delete.SetAlias(t.TableName())
+	return Delete
+}
+
+//BuildCount : build count select query for table
+func (t *Table) BuildCount() *Select {
+	Select := t.NewSelect()
+	Select.Select.Add("count(*)")
+	return Select
+}
+
+//Count : count  from table  by given select t.QueryBuilder().
+func (t *Table) Count(Select *Select) (int, error) {
+	var result int
+	row := Select.QueryRow(t)
+	err := row.Scan(&result)
+	if err != nil {
+		return 0, err
+	}
+	return result, nil
+}
+
+func NewTable(dbtable DBTable) *Table {
+	return &Table{
+		DBTable: dbtable,
+	}
+}
