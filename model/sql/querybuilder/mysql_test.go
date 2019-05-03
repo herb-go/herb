038ec92fd9cda9b0c689353e728bc3ba5f9f3057
Jarlyyn <jarlyyn@qq.com>
2019-05-04 02:19:36 +0800

    update

--- model/sql/querybuilder/mysql_test.go
+++ model/sql/querybuilder/mysql_test.go
@@ -35,8 +35,21 @@ func TestMysql(t *testing.T) {
 		t.Fatal(table1)
 	}
 	builder := table1.QueryBuilder()
-	insertquery := table1.NewInsert()
 	fields := querybuilder.NewFields()
+	var count int
+	selectquery := table1.NewSelect()
+	fields.Set("count(*)", &count)
+	selectquery.Select.AddFields(fields)
+	r := selectquery.QueryRow(table1)
+	err = selectquery.Select.Result().BindFields(fields).ScanFrom(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if count != 0 {
+		t.Fatal(err)
+	}
+	insertquery := table1.NewInsert()
+	fields = querybuilder.NewFields()
 	fields.Set("id", "testid").Set("body", "testbody")
 	insertquery.Insert.AddFields(fields)
 	insertquery.Other = builder.New("ON DUPLICATE KEY UPDATE body= ?", "testbodydup")
