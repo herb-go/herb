a1e713adc8f686cdf2fbb2aec6784b6b80afab13
Jarlyyn <jarlyyn@qq.com>
2019-05-04 15:42:41 +0800

    update

--- model/sql/querybuilder/mysql_test.go
+++ model/sql/querybuilder/mysql_test.go
@@ -22,26 +22,25 @@ func TestMysql(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	_, err = DB.Exec("truncate table testtable1")
-	if err != nil {
-		t.Fatal(err)
-	}
-	_, err = DB.Exec("truncate table testtable2")
-	if err != nil {
-		t.Fatal(err)
-	}
 	table1 := querybuilder.NewTable(DB.Table("testtable1"))
 	if table1.Driver() != "mysql" {
 		t.Fatal(table1)
 	}
+	truncatequery := table1.QueryBuilder().New("truncate table testtable1")
+	truncatequery.MustExec(table1)
+
+	_, err = DB.Exec("truncate table testtable2")
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	builder := table1.QueryBuilder()
 	fields := querybuilder.NewFields()
 	var count int
-	selectquery := table1.NewSelect()
 	fields.Set("count(*)", &count)
-	selectquery.Select.AddFields(fields)
-	r := selectquery.QueryRow(table1)
-	err = selectquery.Select.Result().BindFields(fields).ScanFrom(r)
+	countquery := table1.BuildCount()
+	r := countquery.QueryRow(table1)
+	err = countquery.Result().BindFields(fields).ScanFrom(r)
 	if err != nil {
 		t.Fatal(err)
 	}
