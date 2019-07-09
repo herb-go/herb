0009eaebf44a8066e8832a8bfeecc26fa7148377
Jarlyyn <jarlyyn@qq.com>
2019-07-10 00:15:15 +0800

    update

---
+++ model/sql/db/column/column.go
@@ -0,0 +1,22 @@
+package column
+
+import (
+	"github.com/herb-go/herb/model/sql/db"
+)
+
+var Drivers = map[string]func() ColumnsLoader{}
+
+type Column struct {
+	Field      string
+	ColumnType string
+	AutoValue  bool
+	PrimayKey  bool
+	NotNull    bool
+}
+
+type Columns []Column
+
+type ColumnsLoader interface {
+	Columns() ([]Column, error)
+	Load(conn db.Database, table string) error
+}
