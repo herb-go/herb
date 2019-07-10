c5bb9114d814db779170c3d728d204cc1eb37f44
Jarlyyn <jarlyyn@qq.com>
2019-07-10 20:36:31 +0800

    update

--- model/sql/db/columns/columns_test.go
+++ model/sql/db/columns/columns_test.go
@@ -1,1 +1,33 @@
 package columns
+
+import (
+	"testing"
+
+	"github.com/herb-go/herb/model/sql/db"
+)
+
+type testLoader struct {
+}
+
+func (l *testLoader) Columns() ([]*Column, error) {
+	return nil, nil
+}
+
+func (l *testLoader) Load(conn db.Database, table string) error {
+	return nil
+}
+
+func TestColumns(t *testing.T) {
+	driver := &testLoader{}
+	Register("test", func() ColumnsLoader {
+		return driver
+	})
+	notexist := Driver("notexist")
+	if notexist != nil {
+		t.Fatal()
+	}
+	d := Driver("test")
+	if d == nil || d() != driver {
+		t.Fatal()
+	}
+}
