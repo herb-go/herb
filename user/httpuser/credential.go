0f15fd2e31c96f1e1e55dd451020f88b53d03eaf
Jarlyyn <jarlyyn@qq.com>
2019-11-05 23:35:48 +0800

    update

--- user/httpuser/credential.go
+++ user/httpuser/credential.go
@@ -5,5 +5,5 @@ import "net/http"
 //CredentialProvider credential provider
 type CredentialProvider interface {
 	// credential request with given id
-	Credential(id string, r *http.Request) error
+	Credential(r *http.Request) error
 }
