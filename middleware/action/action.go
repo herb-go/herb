67030e365ebf0726b09fbf64b2b4ed8c7d7dac7f
jarlyyn <jarlyyn@qq.com>
2020-04-10 00:58:23 +0800

    update

---
+++ middleware/action/action.go
@@ -0,0 +1,24 @@
+package action
+
+import "net/http"
+
+//Action action struct
+type Action struct {
+	handler func(w http.ResponseWriter, r *http.Request)
+}
+
+//ServeHTTP serve as http server
+func (a *Action) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	a.handler(w, r)
+}
+
+//SetHandler set action handler
+func (a *Action) SetHandler(f func(w http.ResponseWriter, r *http.Request)) *Action {
+	a.handler = f
+	return a
+}
+
+//Handler return action handler
+func (a *Action) Handler() func(w http.ResponseWriter, r *http.Request) {
+	return a.handler
+}
