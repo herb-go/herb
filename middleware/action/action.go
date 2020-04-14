74851e088ffb72a1602da82989af484bee02313d
jarlyyn <jarlyyn@qq.com>
2020-04-14 23:08:23 +0800

    update

--- middleware/action/action.go
+++ middleware/action/action.go
@@ -1,15 +1,20 @@
 package action
 
-import "net/http"
+import (
+	"net/http"
+	"sync/atomic"
+)
 
 //Action action struct
 type Action struct {
+	visit      *int64
 	middleware func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc)
 	handler    func(w http.ResponseWriter, r *http.Request)
 }
 
 //ServeHTTP serve as http server
 func (a *Action) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	atomic.AddInt64(a.visit, 1)
 	if a.middleware == nil {
 		a.handler(w, r)
 		return
@@ -17,8 +22,13 @@ func (a *Action) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	a.middleware(w, r, a.handler)
 }
 
-//SetHandler set action handler
-func (a *Action) SetHandler(f func(w http.ResponseWriter, r *http.Request)) *Action {
+//SetMiddleware set action middleware
+func (a *Action) SetMiddleware(f func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc)) {
+	a.middleware = f
+}
+
+//WithHandler set action handler
+func (a *Action) WithHandler(f func(w http.ResponseWriter, r *http.Request)) *Action {
 	a.handler = f
 	return a
 }
@@ -26,6 +36,11 @@ func (a *Action) SetHandler(f func(w http.ResponseWriter, r *http.Request)) *Act
 //Handler return action handler
 func (a *Action) Handler() func(w http.ResponseWriter, r *http.Request) {
 	return a.handler
+}
+
+//Visit action visit count
+func (a *Action) Visit() int64 {
+	return atomic.LoadInt64(a.visit)
 }
 
 //New create new cation
