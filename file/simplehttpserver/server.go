b4eccd89e639dbdb25aa4678e11526113b21869f
jarlyyn <jarlyyn@qq.com>
2020-07-22 20:13:08 +0800

    update

--- file/simplehttpserver/server.go
+++ file/simplehttpserver/server.go
@@ -4,12 +4,62 @@ import (
 	"net/http"
 	"os"
 	"path"
+	"path/filepath"
+	"strings"
 )
+
+func isSlashRune(r rune) bool { return r == '/' || r == '\\' }
+
+func containsDotDot(v string) bool {
+	if !strings.Contains(v, "..") {
+		return false
+	}
+	for _, ent := range strings.FieldsFunc(v, isSlashRune) {
+		if ent == ".." {
+			return true
+		}
+	}
+	return false
+}
+func renderError(w http.ResponseWriter, err error) {
+
+	if os.IsNotExist(err) {
+		http.Error(w, http.StatusText(404), 404)
+	} else if os.IsPermission(err) {
+		http.Error(w, http.StatusText(404), 403)
+	} else {
+		panic(err)
+	}
+}
+func serveFile(w http.ResponseWriter, r *http.Request, path string) {
+	if containsDotDot(r.URL.Path) {
+		return
+	}
+	// dir, file := filepath.Split(name)
+	// serveFile(w, r, Dir(dir), file, false)
+	stat, err := os.Stat(path)
+	if err != nil {
+		renderError(w, err)
+		return
+	}
+	if stat.IsDir() {
+		http.Error(w, http.StatusText(404), 403)
+		return
+	}
+	file, err := os.Open(path)
+	if err != nil {
+		renderError(w, err)
+		return
+	}
+	defer file.Close()
+	name := filepath.Base(path)
+	http.ServeContent(w, r, name, stat.ModTime(), file)
+}
 
 //ServeFile serve given file as http resopnse.
 func ServeFile(path string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, path)
+		serveFile(w, r, path)
 	}
 }
 
@@ -19,16 +69,17 @@ func ServeFile(path string) http.HandlerFunc {
 func ServeFolder(root string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		file := path.Join(root, r.URL.Path)
-		fi, err := os.Stat(file)
-		if err == nil {
-			if fi.IsDir() {
-				ii, err := os.Stat(path.Join(file, "index.html"))
-				if err != nil || ii.IsDir() {
-					http.Error(w, http.StatusText(403), 403)
-					return
-				}
+		stat, err := os.Stat(file)
+		if err != nil {
+			renderError(w, err)
+			return
+		}
+		if stat.IsDir() {
+			filestat, err := os.Stat(path.Join(file, "index.html"))
+			if err == nil && !filestat.IsDir() {
+				file = path.Join(file, "index.html")
 			}
 		}
-		http.ServeFile(w, r, file)
+		serveFile(w, r, file)
 	}
 }
