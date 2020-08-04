e4423aa45a4863155cfe4113930c348868f7ed67
jarlyyn <jarlyyn@qq.com>
2020-08-04 08:07:28 +0800

    update

--- middleware/misc/method.go
+++ middleware/misc/method.go
@@ -17,3 +17,30 @@ func MethodMiddleware(methods ...string) func(w http.ResponseWriter, r *http.Req
 		http.Error(w, http.StatusText(405), 405)
 	}
 }
+
+//Method http request method middleware type
+type Method string
+
+//ServeMiddleware serve as middleware
+func (m Method) ServeMiddleware(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	if r.Method != string(m) {
+		http.Error(w, http.StatusText(405), 405)
+		return
+	}
+	next(w, r)
+}
+
+//MethodPOST method post only middleare
+var MethodPOST = Method("POST")
+
+//MethodGET method get only middleare
+var MethodGET = Method("GET")
+
+//MethodPUT method put only middleare
+var MethodPUT = Method("PUT")
+
+//MethodDELETE method delete only middleare
+var MethodDELETE = Method("DELETE")
+
+//MethodOPTIONS method options only middleare
+var MethodOPTIONS = Method("OPTIONS")
--- middleware/misc/method_test.go
+++ middleware/misc/method_test.go
@@ -36,3 +36,125 @@ func TestMethod(t *testing.T) {
 	}
 
 }
+
+func TestMethos(t *testing.T) {
+	var successAction = func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("ok"))
+		if err != nil {
+			panic(err)
+		}
+	}
+	mux := http.ServeMux{}
+	mux.HandleFunc("/GET", func(w http.ResponseWriter, r *http.Request) {
+		MethodGET.ServeMiddleware(w, r, successAction)
+	})
+	mux.HandleFunc("/POST", func(w http.ResponseWriter, r *http.Request) {
+		MethodPOST.ServeMiddleware(w, r, successAction)
+	})
+	mux.HandleFunc("/PUT", func(w http.ResponseWriter, r *http.Request) {
+		MethodPUT.ServeMiddleware(w, r, successAction)
+	})
+	mux.HandleFunc("/DELETE", func(w http.ResponseWriter, r *http.Request) {
+		MethodDELETE.ServeMiddleware(w, r, successAction)
+	})
+	mux.HandleFunc("/OPTIONS", func(w http.ResponseWriter, r *http.Request) {
+		MethodOPTIONS.ServeMiddleware(w, r, successAction)
+	})
+	s := httptest.NewServer(&mux)
+	defer s.Close()
+	resp, err := http.DefaultClient.Get(s.URL + "/GET")
+	if err != nil {
+		panic(err)
+	}
+	resp.Body.Close()
+	if resp.StatusCode != 200 {
+		t.Fatal(resp)
+	}
+	resp, err = http.DefaultClient.Post(s.URL+"/GET", "", nil)
+	if err != nil {
+		panic(err)
+	}
+	resp.Body.Close()
+	if resp.StatusCode != 405 {
+		t.Fatal(resp)
+	}
+	resp, err = http.DefaultClient.Post(s.URL+"/POST", "", nil)
+	if err != nil {
+		panic(err)
+	}
+	resp.Body.Close()
+	if resp.StatusCode != 200 {
+		t.Fatal(resp)
+	}
+	resp, err = http.DefaultClient.Get(s.URL + "/POST")
+	if err != nil {
+		panic(err)
+	}
+	resp.Body.Close()
+	if resp.StatusCode != 405 {
+		t.Fatal(resp)
+	}
+
+	req, err := http.NewRequest("PUT", s.URL+"/PUT", nil)
+	if err != nil {
+		panic(err)
+	}
+	resp, err = http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	resp.Body.Close()
+	if resp.StatusCode != 200 {
+		t.Fatal(resp)
+	}
+
+	resp, err = http.DefaultClient.Get(s.URL + "/PUT")
+	if err != nil {
+		panic(err)
+	}
+	resp.Body.Close()
+	if resp.StatusCode != 405 {
+		t.Fatal(resp)
+	}
+
+	req, err = http.NewRequest("DELETE", s.URL+"/DELETE", nil)
+	if err != nil {
+		panic(err)
+	}
+	resp, err = http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	resp.Body.Close()
+	if resp.StatusCode != 200 {
+		t.Fatal(resp)
+	}
+	resp, err = http.DefaultClient.Get(s.URL + "/DELETE")
+	if err != nil {
+		panic(err)
+	}
+	resp.Body.Close()
+	if resp.StatusCode != 405 {
+		t.Fatal(resp)
+	}
+	req, err = http.NewRequest("OPTIONS", s.URL+"/OPTIONS", nil)
+	if err != nil {
+		panic(err)
+	}
+	resp, err = http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	resp.Body.Close()
+	if resp.StatusCode != 200 {
+		t.Fatal(resp)
+	}
+	resp, err = http.DefaultClient.Get(s.URL + "/OPTIONS")
+	if err != nil {
+		panic(err)
+	}
+	resp.Body.Close()
+	if resp.StatusCode != 405 {
+		t.Fatal(resp)
+	}
+}
