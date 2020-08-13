83b0292fa1185008d525a5e9cdf7d7126dfdc438
jarlyyn <jarlyyn@qq.com>
2020-08-13 23:59:32 +0800

    update

--- user/password.go
+++
@@ -1,8 +0,0 @@
-package user
-
-//PasswordVerifier password verifier interface
-type PasswordVerifier interface {
-	//VerifyPassword Verify user password.
-	//Return verify result and any error if raised
-	VerifyPassword(uid string, password string) (bool, error)
-}
