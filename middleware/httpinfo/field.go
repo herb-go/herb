6b98bffcbb4cc7c2bd48ab2b2364d01dfe9f37ce
jarlyyn <jarlyyn@qq.com>
2021-02-04 00:05:15 +0800

    update

--- middleware/httpinfo/field.go
+++ middleware/httpinfo/field.go
@@ -15,3 +15,22 @@ type FieldFunc func(r *http.Request) ([]byte, bool, error)
 func (f FieldFunc) LoadInfo(r *http.Request) ([]byte, bool, error) {
 	return f(r)
 }
+
+//StringField string field
+type StringField struct {
+	Field Field
+}
+
+func (f *StringField) LoadStringInfo(r *http.Request) (string, error) {
+	data, _, err := f.Field.LoadInfo(r)
+	if err != nil {
+		return "", err
+	}
+	return string(data), nil
+}
+
+func NewStringField(f Field) *StringField {
+	return &StringField{
+		Field: f,
+	}
+}
